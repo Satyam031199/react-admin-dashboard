bd35907c183f92db6296b203158517112fcf0615
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2024-07-28 17:44:08 +0530

    Dashboard completed

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>Admin Panel</span>
+      <span>Satyam Chaturvedi Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
